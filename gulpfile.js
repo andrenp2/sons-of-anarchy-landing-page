d13b2dc5960163f601f01c148471e4d0cb60c260
Andre Pavan <dehnuernberg@hotmail.com>
2024-06-03 15:38:15 -0300

    add: configurações do ambiente

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// definir os pacotes importados
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// definir função
+
+// função para comprimir os arquivos .css
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = gulp.parallel(styles);
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+}
